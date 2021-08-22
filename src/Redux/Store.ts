9ee819855b7ec5a718dcf336c951f340053b86fd
Vinayak1337 <vinayak111kumar@gmail.com>
2021-08-22 17:04:09 +0530

    updating project

--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -1,10 +1,13 @@
-import { applyMiddleware, createStore } from "redux";
-import logger from 'redux-logger';
+import { createStore } from "redux";
+// import logger from 'redux-logger';
 
 import rootReducer from './RootReducer';
 
-const middlewares = [ logger ];
+// const middlewares = [ logger ];
 
-const Store = createStore(rootReducer, applyMiddleware(...middlewares));
+// const Store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+const Store = createStore(rootReducer);
+
 
 export default Store;
