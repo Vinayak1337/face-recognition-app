f8bd7774cda90f99af0a4061402740141c9bc07e
Vinayak1337 <34209962+Vinayak1337@users.noreply.github.com>
2023-10-21 15:14:18 +0530

    added a fetch request to wake up the server

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,26 +1,43 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { Header } from '../Components/Header';
 import { Home, Profile, Auth, ErrorPage } from '../Pages';
 import './App.css';
+import { baseUrl } from '../Assets/data';
 
 const App: FC<AppProps> = ({ user }) => {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path='/' render={() => (!user ? <Redirect to="/signin" /> : <Home /> )} />
-        <Route exact path='/profile' render={() => (!user ? <Redirect to="/signin" /> : <Profile /> )} />
-        <Route exact path='/signin' render={() => (user ? <Redirect to="/" /> : <Auth /> )} />
-        <Route component={ErrorPage} />
-      </Switch>
-    </div>
-  );
-}
+	useEffect(() => {
+		fetch(baseUrl); // to wake up the server
+	}, []);
+
+	return (
+		<div className='App'>
+			<Header />
+			<Switch>
+				<Route
+					exact
+					path='/'
+					render={() => (!user ? <Redirect to='/signin' /> : <Home />)}
+				/>
+				<Route
+					exact
+					path='/profile'
+					render={() => (!user ? <Redirect to='/signin' /> : <Profile />)}
+				/>
+				<Route
+					exact
+					path='/signin'
+					render={() => (user ? <Redirect to='/' /> : <Auth />)}
+				/>
+				<Route component={ErrorPage} />
+			</Switch>
+		</div>
+	);
+};
 
 const mapStateToProps = (state: RootState) => ({
-  user: state.userReducer.user
+	user: state.userReducer.user
 });
 
 export default connect(mapStateToProps)(App);
