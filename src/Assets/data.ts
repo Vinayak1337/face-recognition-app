734d0b4bbf71694b88b7ce0ce9a8aa667fa6a6d6
Vinayak1337 <34209962+Vinayak1337@users.noreply.github.com>
2023-10-21 14:18:04 +0530

    fixed a few things and made the project up and running

--- src/Assets/data.ts
+++ src/Assets/data.ts
@@ -1,69 +1,69 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-export const baseUrl = 'https://image-recognition-server.herokuapp.com';
-// export const baseUrl = 'http://localhost:8080';
+export const baseUrl =
+	process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
 
 export const globalOptions: FetchOptions = {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    }
-}
+	method: 'GET',
+	headers: {
+		'Content-Type': 'application/json'
+	}
+};
 
 export const setToLocal = (key: string, value: any) => {
-    try {
-        const string = value != null ? JSON.stringify(value) : null;
-        if (!string) return localStorage.removeItem(key);
+	try {
+		const string = value != null ? JSON.stringify(value) : null;
+		if (!string) return localStorage.removeItem(key);
 
-        localStorage.setItem(key, string);
-    } catch (error) {
-        throw error;
-    }
-}
+		localStorage.setItem(key, string);
+	} catch (error) {
+		throw error;
+	}
+};
 
 export const getFromLocal = (key: string, initialValue: any) => {
-    return localStorage.getItem(key) || initialValue;
-}
+	return localStorage.getItem(key) || initialValue;
+};
 
 export const setToSession = (key: string, value: any) => {
-    try {
-        const string = (value != null) ? JSON.stringify(value) : null;
-        if (!string) return sessionStorage.removeItem(key);
+	try {
+		const string = value != null ? JSON.stringify(value) : null;
+		if (!string) return sessionStorage.removeItem(key);
 
-        sessionStorage.setItem(key, string);
-    } catch (error) {
-        throw error;
-    }
-}
+		sessionStorage.setItem(key, string);
+	} catch (error) {
+		throw error;
+	}
+};
 
 export const getFromSession = (key: string, initialValue: any) => {
-    return sessionStorage.getItem(key) || initialValue;
-}
+	return sessionStorage.getItem(key) || initialValue;
+};
 
 export const FLAGS = {
-    HAS_LOCAL_USER: false,
-}
+	HAS_LOCAL_USER: false
+};
 
-export function useSession<A> (key: string, initialValue: A) {
-    let [value, setValue] = useState(() => {
-        return getFromSession(key, initialValue);
-    });
+export function useSession<A>(key: string, initialValue: A) {
+	let [value, setValue] = useState(() => {
+		return getFromSession(key, initialValue);
+	});
 
-    useEffect(() => setToSession(key, value), [key, value]);
+	useEffect(() => setToSession(key, value), [key, value]);
 
-    return [value, setValue];
+	return [value, setValue];
 }
 
 export function useLocalStorage<A>(key: string, initialValue: A) {
-    let [value, setValue] = useState(() => {
-        return getFromLocal(key, initialValue);
-    });
+	let [value, setValue] = useState(() => {
+		return getFromLocal(key, initialValue);
+	});
 
-    useEffect(() => setToLocal(key, value), [key, value]);
+	useEffect(() => setToLocal(key, value), [key, value]);
 
-    return [value, setValue];
+	return [value, setValue];
 }
 
 export const useLogger = (...args: any) => {
-    useEffect(() => console.log(args), [args]);
-}
+	useEffect(() => console.log(args), [args]);
+};
--- src/Components/Home/ImageForm/ImageForm.tsx
+++ src/Components/Home/ImageForm/ImageForm.tsx
@@ -1,190 +1,218 @@
-import { ChangeEvent, FC, useReducer } from 'react'
+import { ChangeEvent, FC, useReducer, useRef } from 'react';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
-import { baseUrl } from '../../../Assets/data'
+import { baseUrl } from '../../../Assets/data';
 import { incrementEntries } from '../../../Redux/User/UserActions';
 import MessageBar from '../../Global/MessageBar/MessageBar';
-import './ImageForm.scss'
+import './ImageForm.scss';
 
 const SET_INPUT = 'set_input',
-    SET_IS_URL = 'set_is_url',
-    SET_IS_IMAGE = 'set_is_image',
-    SET_FACE_DATA = 'set_face_data',
-    SET_HAS_INPUT = 'set_has_input',
-    SET_ACTIVE_BOOLEAN = 'set_active_boolean';
+	SET_IS_URL = 'set_is_url',
+	SET_IS_IMAGE = 'set_is_image',
+	SET_FACE_DATA = 'set_face_data',
+	SET_HAS_INPUT = 'set_has_input',
+	SET_ACTIVE_BOOLEAN = 'set_active_boolean';
 
 type BOOLEANS = typeof SET_IS_URL | typeof SET_IS_IMAGE | typeof SET_HAS_INPUT;
 
 const ImageForm: FC<ImageFormProps> = ({ incrementEntries, user }) => {
-    const [state, dispatch] = useReducer(stateReducer, {
-        input: '',
-        isUrl: true,
-        isImage: true,
-        hasInput: true,
-        faceData: null,
-        activeBoolean: null
-    });
+	const [state, dispatch] = useReducer(stateReducer, {
+		input: '',
+		isUrl: true,
+		isImage: true,
+		hasInput: true,
+		faceData: null,
+		activeBoolean: null
+	});
 
-    const calculateFaceBox = () => {
-        const faces = state.faceData?.outputs[0].data.regions.map(region => region.region_info.bounding_box)
-        if (!faces) return;
+	const imgRef = useRef<HTMLImageElement>(null);
 
-        const image = document.getElementById('image-form-imageBody')! as HTMLImageElement;
-        const imageWidth = Number(image.width);
-        const imageHeight = Number(image.height);
-        console.log(imageWidth, imageHeight);
+	const calculateFaceBox = () => {
+		if (!state.faceData) return;
 
-        console.log(faces.map(face => ({
-            leftCol: face.left_col,
-            rightCol: face.right_col,
-            topRow: face.top_row,
-            bottomRow: face.bottom_row
-        })));
+		const faces = state.faceData.outputs[0].data.regions.map(
+			region => region.region_info.bounding_box
+		);
 
-        const boxes: FaceBox[] = faces.map(face => ({
-            leftCol: imageWidth * face.left_col,
-            topRow: face.top_row * imageHeight,
-            rightCol: imageWidth - ( face.right_col * imageWidth),
-            bottomRow: imageHeight - (face.bottom_row * imageHeight)
-        }))
+		if (!faces || !imgRef.current) return;
 
-        return (
-            <>
-            {
-                boxes.map((face, index) => {
-                    return <div key={index} className='bounding-box' 
-                    style={{top: `${face.topRow}px`, right: `${face.rightCol}px`, bottom: `${face.bottomRow}px`, left: `${face.leftCol}px`,
-                    width: `${imageWidth - (face.rightCol + face.leftCol)}px`, height: `${ imageHeight - (face.bottomRow + face.topRow)}px`}}></div>
-                })
-            }
-            </>
-        );
-    }
+		const imageWidth = Number(imgRef.current.width);
+		const imageHeight = Number(imgRef.current.height);
+		console.log(imageWidth, imageHeight);
 
-    const getErrorMessage = (optional?: string) => {
-        if (!state.hasInput) return 'Please enter an image url';
-        else if (!state.isUrl) return 'Given input is not an url';
-        else if (!state.isImage) return 'Given input is not an image url';
-        else if (!optional) return 'Something went wrong!';
-        return optional;
-    }
+		console.log(
+			faces.map(face => ({
+				leftCol: face.left_col,
+				rightCol: face.right_col,
+				topRow: face.top_row,
+				bottomRow: face.bottom_row
+			}))
+		);
 
-    const handleSubmit = async () => {
-        const { input } = state;
+		const boxes: FaceBox[] = faces.map(face => ({
+			leftCol: imageWidth * face.left_col,
+			topRow: face.top_row * imageHeight,
+			rightCol: imageWidth - face.right_col * imageWidth,
+			bottomRow: imageHeight - face.bottom_row * imageHeight
+		}));
 
-        if (!input.length) return toggleState(SET_HAS_INPUT);
+		return (
+			<>
+				{boxes.map((face, index) => (
+					<div
+						key={index}
+						className='bounding-box'
+						style={{
+							top: `${face.topRow}px`,
+							right: `${face.rightCol}px`,
+							bottom: `${face.bottomRow}px`,
+							left: `${face.leftCol}px`,
+							width: `${imageWidth - (face.rightCol + face.leftCol)}px`,
+							height: `${imageHeight - (face.bottomRow + face.topRow)}px`
+						}}
+					/>
+				))}
+			</>
+		);
+	};
 
-        const res2 = await fetch(baseUrl + '/fetchimage', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*"
-            },
-            body: JSON.stringify({ imageUrl: input })
-        });
+	const getErrorMessage = (optional?: string) => {
+		if (!state.hasInput) return 'Please enter an image url';
+		else if (!state.isUrl) return 'Given input is not an url';
+		else if (!state.isImage) return 'Given input is not an image url';
+		else if (!optional) return 'Something went wrong!';
+		return optional;
+	};
 
-        if (res2.status === 200) {
-            const faceData = await res2.json() as FaceData;
+	const handleSubmit = async () => {
+		const { input } = state;
 
-            dispatch({
-                type: SET_FACE_DATA,
-                payload: faceData,
-            })
+		if (!input.length) return toggleState(SET_HAS_INPUT);
 
-            const res3 = await fetch(baseUrl + '/image', {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "*",
-                },
-                body: JSON.stringify({ image: input, id: user?.id })
-            })
+		const res = await fetch(baseUrl + '/fetchimage', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*'
+			},
+			body: JSON.stringify({ imageUrl: input })
+		});
 
-            if (res3.status === 200) {
-                incrementEntries()
-            }
+		if (res.status === 200) {
+			const faceData = (await res.json()) as FaceData;
 
-            return;
-        }
+			dispatch({
+				type: SET_FACE_DATA,
+				payload: faceData
+			});
 
-        if (res2.status === 500) toggleState(SET_IS_IMAGE);
-    }
+			const res2 = await fetch(baseUrl + '/image', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*'
+				},
+				body: JSON.stringify({ image: input, id: user?.id })
+			});
+			if (res2.status === 200) incrementEntries();
 
-    const toggleState = (stateType: BOOLEANS) => {
-        dispatch({
-            type: stateType,
-            payload: false,
-        });
-        dispatch({ type: SET_ACTIVE_BOOLEAN, payload: stateType });
-        dispatch({ type: SET_FACE_DATA, payload: null });
+			return;
+		}
 
-        setTimeout(() => {
-            dispatch({ type: stateType, payload: true })
-            dispatch({ type: SET_ACTIVE_BOOLEAN, payload: null });
-        }, 3000);
-    }
+		if (res.status === 500) toggleState(SET_IS_IMAGE);
+	};
 
-    return (
-        <>
-        <div className="image-form-body">
-            <div className="image-form-border">
-                <input value={state.input} onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                    dispatch({
-                        type: SET_INPUT,
-                        payload: event.target.value
-                    })
-                    if (state.faceData) {
-                        dispatch({ type: SET_FACE_DATA, payload: null })
-                    }
-                }} />
-                <button type="button" onClick={handleSubmit}>DETECT</button>
-            </div>
-            <div className="image-form-image-container">
-             <img id="image-form-imageBody" src={state.input} alt="" />
-            {
-                state.input ? calculateFaceBox() : ''
-            }
-            </div>
-            { state.activeBoolean ? <MessageBar type="error" message={getErrorMessage()} handleClose={() => {
-                if (!state.activeBoolean) return;
-                dispatch({ type: SET_ACTIVE_BOOLEAN, payload: null });
-            }} /> : ''}
-        </div>
-        </>
-    )
-}
+	const toggleState = (stateType: BOOLEANS) => {
+		dispatch({
+			type: stateType,
+			payload: false
+		});
+		dispatch({ type: SET_ACTIVE_BOOLEAN, payload: stateType });
+		dispatch({ type: SET_FACE_DATA, payload: null });
+
+		setTimeout(() => {
+			dispatch({ type: stateType, payload: true });
+			dispatch({ type: SET_ACTIVE_BOOLEAN, payload: null });
+		}, 3000);
+	};
+
+	return (
+		<>
+			<div className='image-form-body'>
+				<div className='image-form-border'>
+					<input
+						value={state.input}
+						onChange={(event: ChangeEvent<HTMLInputElement>) => {
+							dispatch({
+								type: SET_INPUT,
+								payload: event.target.value
+							});
+							if (state.faceData) {
+								dispatch({ type: SET_FACE_DATA, payload: null });
+							}
+						}}
+					/>
+					<button type='button' onClick={handleSubmit}>
+						DETECT
+					</button>
+				</div>
+				<div className='image-form-image-container'>
+					<img
+						id='image-form-imageBody'
+						ref={imgRef}
+						src={state.input}
+						alt=''
+					/>
+					{state.faceData ? calculateFaceBox() : ''}
+				</div>
+				{state.activeBoolean ? (
+					<MessageBar
+						type='error'
+						message={getErrorMessage()}
+						handleClose={() => {
+							if (!state.activeBoolean) return;
+							dispatch({ type: SET_ACTIVE_BOOLEAN, payload: null });
+						}}
+					/>
+				) : (
+					''
+				)}
+			</div>
+		</>
+	);
+};
 
 function stateReducer(state: ImageFormState, action: ImageFormAction) {
-    switch (action.type) {
-        case SET_INPUT:
-            return { ...state, input: action.payload }
+	switch (action.type) {
+		case SET_INPUT:
+			return { ...state, input: action.payload };
 
-        case SET_IS_URL:
-            return { ...state, isURL: action.payload }
-        
-        case SET_IS_IMAGE:
-            return { ...state, isImage: action.payload }
+		case SET_IS_URL:
+			return { ...state, isURL: action.payload };
 
-        case SET_HAS_INPUT:
-            return { ...state, hasInput: action.payload }
+		case SET_IS_IMAGE:
+			return { ...state, isImage: action.payload };
 
-        case SET_FACE_DATA:
-            return { ...state, faceData: action.payload }
+		case SET_HAS_INPUT:
+			return { ...state, hasInput: action.payload };
 
-        case SET_ACTIVE_BOOLEAN:
-            return { ...state, activeBoolean: action.payload }
+		case SET_FACE_DATA:
+			return { ...state, faceData: action.payload };
 
-        default:
-            return state;
-    }
+		case SET_ACTIVE_BOOLEAN:
+			return { ...state, activeBoolean: action.payload };
+
+		default:
+			return state;
+	}
 }
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-    incrementEntries: () => dispatch(incrementEntries())
+	incrementEntries: () => dispatch(incrementEntries())
 });
 
 const mapStateToProps = (state: RootState) => ({
-    user: state.userReducer.user,
+	user: state.userReducer.user
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ImageForm)
+export default connect(mapStateToProps, mapDispatchToProps)(ImageForm);
